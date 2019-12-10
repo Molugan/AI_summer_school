f9cbb79ab9f62487bd00302ac7b464214a796636
Morgane Rivière <mriviere@fb.com>
2019-12-10 16:21:31 +0100

    DOC: 1_tensor_operators_exercice -> meilleure documentation

--- TD_1/1_tensor_operators_exercice.py
+++ TD_1/1_tensor_operators_exercice.py
@@ -15,21 +15,30 @@ def sum_x_y(x, y):
 # tensor x
 
 def norm(x):
+    r"""
+    Argument :
+        x un tenseur de taille arbitraire
+    Remvoie:
+        sqrt(sum (x_i * x_i)) où x_i sont les éléments de x
+    """
     # Votre code ici
     return
 
 # Maintenant considérons une série de K vecteurs de dimension D.
 # La manière standard de représenter ces données est par un tensor de dimension
-# K x D. Définissez une fonction qui prend en entrée un tel vecteur x et
+# K x D. Définissez une fonction qui prend en entrée un tel tensor X et
 # retourne un vecteur de dimension K, contenant la norme L2 des K "sous-vecteurs"
-# de x.
+# de X.
 
 def partial_norm(x):
+    r"""
+    Argument :
+        X un tensor de taille K x D
+    Renvoie:
+        un tensor Nx de taille K tel que Nx[i] = sqrt(sum(X[i, j] * X[i, j]))
+    """
     # Votre code ici
     return
-
-
-# De la même manière,
 
 
 # Exercice 2:
@@ -40,29 +49,47 @@ def partial_norm(x):
 # N x S x D  !
 # La première dimension est appelée le batch, la deuxième est le temps, et la
 # troisième le cannal.
-# Nous voulons donc calculer pour chaque séquence x, le vecteur moyen x_mu (de dimension D)
-# x sur l'ensemble de ses S vecteurs ainsi que sa variance x_sigma (de dimension D) sur chacune de
+# Nous voulons donc calculer pour chaque séquence x, le vecteur moyen mu (de dimension D)
+# x sur l'ensemble de ses S vecteurs ainsi que sa variance sigma (de dimension D) sur chacune de
 # ses dimension.
 
-# commencez par écrire les fonctions permettant de calculer x_mu et x_sigma
-# Les fonctions
+# Commencez par écrire les fonctions permettant de calculer mu et sigma
 
-def get_mean_on_seq(x):
+def get_mean_on_seq(batch):
+    r"""
+    Argument :
+        batch : un tensor de taille N x S x D
+    Renvoie:
+        Un tensor mu de dimension N x D telle que mu[n, d] = moyenne_{s}(x[n, s, d])
+    """
     # Votre code ici: x_mu
     return
 
-def get_var_seq(x):
+def get_var_seq(batch):
+    r"""
+    Argument :
+        batch : un tensor de taille N x S x D
+    Renvoie:
+        Un tensor sigma de dimension N x D telle que sigma[n, d] = variance_{s}(x[n, s, d])
+    """
     # Votre code ici: x_mu
     return
 
 # Nous voulons à présent que chaque séquence x ait une moyenne nulle et une variance 1
-# en d'autres termes nous voulons calculer : x - x_mu / sqrt(epsilon + x_sigma)
+# en d'autres termes nous voulons calculer : x - mu / sqrt(epsilon + sigma)
 # epsilon est une petite valeur positive pour éviter de diviser par zero.
 # On peut prendre par exemple epsilon = 1e-8
 # Pour éviter d'avoir une erreur, ajoutez le paramètre keepdim=True dans la
 # methode sum()
 
-def instance_norm(x):
+def instance_norm(batch):
+    r"""
+    Argument :
+        batch : un tensor de taille N x S x D
+    Renvoie:
+        Un tensor norm_batch de taille NxSxD tel que
+        norm_batch[n, s, d] = batch[n,s,d] - mu[n, d] / sqrt(epsilon + sigma[n, d])
+    """
     # Votre code ici
     return
 
@@ -75,6 +102,13 @@ def instance_norm(x):
 # pytorch. Elles s'utilisent de la même manière que .sum()
 
 def batch_norm(x):
+    r"""
+    Argument :
+        batch : un tensor de taille N x S x D
+    Renvoie:
+        Un tensor norm_batch de taille NxSxD tel que
+        norm_batch[n, s, d] = batch[n,s,d] - mu[d] / sqrt(epsilon + sigma[d])
+    """
     # Votre code ici
     return
 
@@ -89,10 +123,17 @@ def batch_norm(x):
 # 2) si x est de dimension NxS et y est de dimension N alors x[y>0] vous donnera
 # toutes les séquences de xi (de dimension S) de telles que yi > 0
 
-def kMEANS(x):
+def kMEANS(x, k, n_iterations):
+    r"""
+    Argument :
+        x : un tensor de taille N x S
+        k : nombre de clusters à calculer
+        n_iterations : nombre maximal d'iterations
+    Renvoie:
+        k clusters
+    """
     # Votre code ici
     return
-
 
 
 if __name__ == "__main__":
